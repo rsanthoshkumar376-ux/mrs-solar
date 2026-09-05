d96ae99f7190df531deeced9a377afe77e061c3e
MRS SOLAR <admin@mrs-solar.com>
2026-09-05 15:16:48 +0530

    CustomerEditRestrictedToAdminAndEmailFix

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -9,9 +9,6 @@ const getTransporter = () => {
 
   return nodemailer.createTransport({
     service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
     auth: {
       user,
       pass
